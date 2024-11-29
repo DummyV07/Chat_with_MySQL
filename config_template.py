4d145c882484a9cd43223a2166a343e80a9e3143
DummyV07 <dummy.v07@gmail.com>
2024-11-29 11:44:19 +0800

    添加可用模型列表

--- config_template.py
+++ config_template.py
@@ -9,16 +9,24 @@ BASE_URL_BAILIAN = "https://dashscope.aliyuncs.com/compatible-mode/v1"
 
 MODELS_SQL = [
     "qwen2.5-14b-instruct",
+    "qwen2.5-32b-instruct",
+    "qwen2.5-72b-instruct",
+    "qwen-plus",
     "qwen2.5-coder-7b-instruct",
     "qwen2.5-coder-14b-instruct",
     "qwen2.5-coder-32b-instruct",
+    
     ]
 
 MODELS_NL = [
     
     "qwen2.5-14b-instruct",
     "qwen2.5-32b-instruct",
+    "qwen2.5-72b-instruct",
     "qwen-plus",
+    "qwen2.5-coder-7b-instruct",
+    "qwen2.5-coder-14b-instruct",
+    "qwen2.5-coder-32b-instruct",
 ]
 
 
